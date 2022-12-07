acf8dd6cdd745b514b71533ac5839770e9e63609
Loromirov <loromirov@yandex.ru>
2022-12-07 23:30:04 +0300

    вторая задача

--- Program.cs
+++ Program.cs
@@ -1,13 +1,18 @@
 ﻿/*
-Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+ Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-456 -> 5
-782 -> 8
-918 -> 1
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
 */
 
-Console.WriteLine("Введите трехзначное число");
-int number = Convert.ToInt32(Console.ReadLine());
-int twoDigitNum = number / 10;
-int finNum = twoDigitNum % 10;
-Console.WriteLine(finNum);
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+string str = num.ToString();
+if(num / 100 != 0 )
+{
+    Console.WriteLine(str[2]);
+}
+else Console.WriteLine("В числе нет третьей цифры");
