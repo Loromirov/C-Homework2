0ca1e79d91dae469c9aa2111620da1b0586955b7
Loromirov <loromirov@yandex.ru>
2022-12-07 23:39:08 +0300

    третья задача

--- Program.cs
+++ Program.cs
@@ -1,18 +1,15 @@
 ﻿/*
- Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
-645 -> 5
-
-78 -> третьей цифры нет
-
-32679 -> 6
+6 -> да
+7 -> да
+1 -> нет
 */
 
-Console.WriteLine("Введите число");
-int num = Convert.ToInt32(Console.ReadLine());
-string str = num.ToString();
-if(num / 100 != 0 )
+Console.WriteLine("Введите число от 1 до 7");
+int day = Convert.ToInt32(Console.ReadLine());
+if(day == 6 || day == 7)
 {
-    Console.WriteLine(str[2]);
+    Console.WriteLine("Да");
 }
-else Console.WriteLine("В числе нет третьей цифры");
+    else Console.WriteLine("Нет");
